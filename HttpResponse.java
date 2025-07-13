8e2933e4ee0d12be0d98d11d0caf51b8d0c65977
Harshit-dell <harshitsahu967@gmail.com>
2025-07-13 18:28:59 +0530

    Delete HttpResponse.java

--- HttpResponse.java
+++
@@ -1,9 +0,0 @@
-package com.Learning;
-
-import java.util.List;
-import java.util.Map;
-
-record HttpResponse(int responseCode,
-                    Map<String, List<String>> headers,
-                    byte[] body) {
-}
